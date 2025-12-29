b452951122eafe19a9ffbe6cee186e3bb3f0c5e7
Hyromy <formulajoel9@gmail.com>
2025-12-28 22:08:44 -0600

    use state

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
+
 type ListProps = {
     data: string[]
 }
 export default function List(props: ListProps) {
+    const [index, setIndex] = useState(-1)
     const {data} = props
+    const handleClick = (i: number) => {
+        setIndex(i)
+    }
     return <ul className="list-group">
         {data.map((item, i) => {
-            return <li key={i} className="list-group-item">{item}</li>
+            return <li 
+                onClick={() => handleClick(i)}
+                key={i}
+                className={`list-group-item ${index == i ? "active" : ""}`}>
+                    {item}
+            </li>
         })}
     </ul>
 }
